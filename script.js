c622128301ed005a143806e4af694f54e86c29ba
wesleycs95 <wesleycs95@gmail.com>
2023-09-08 20:17:59 -0300

    teste

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcular (){
+    var Elementnum1 = document.getElementById('num1')
+    var Elementnum2 = document.getElementById('num2')
+    var num1 = Number(Elementnum1.value)
+    var num2 = Number(Elementnum2.value)
+
+    var res = document.getElementById('resultado')
+
+    for( num1= num1 ; num1 <= num2 ; num1++) {
+        
+    } res.innerHTML = setTimeout(num1, 100)
+    
+}
